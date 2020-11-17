f98ee562973bd754311ebef5b8c6037b6399b2d1
ChuhanZhou <60944082+ChuhanZhou@users.noreply.github.com>
2020-11-17 01:14:47 +0100

    update

--- SEP3_ChatSystem/Model/List/Group/ChatGroupList.cs
+++ SEP3_ChatSystem/Model/List/Group/ChatGroupList.cs
@@ -36,5 +36,74 @@ namespace SEP3_ChatSystem.Model.List.Group
             }
             return false;
         }
+
+        public int GetSize()
+        {
+            return GroupList.Count;
+        }
+
+        public ChatGroup GetGroupByIndex(int index)
+        {
+            if (index>=0&&index<GroupList.Count)
+            {
+                return GroupList[0];
+            }
+
+            return null;
+        }
+
+        public ChatGroup GetGroupByGroupId(string id)
+        {
+            foreach (var group in GroupList)
+            {
+                if (group.GroupId==id)
+                {
+                    return group;
+                }
+            }
+            return null;
+        }
+
+        public ChatGroupList GetGroupByCreatorId(string id)
+        {
+            ChatGroupList chatGroupList = new ChatGroupList();
+            foreach (var group in GroupList)
+            {
+                if (group.CreatorId==id)
+                {
+                    chatGroupList.AddNewGroup(group);
+                }
+            }
+            return chatGroupList;
+        }
+        
+        public ChatGroupList GetGroupById(string id)
+        {
+            ChatGroupList chatGroupList = new ChatGroupList();
+            foreach (var group in GroupList)
+            {
+                if (group.HasId(id))
+                {
+                    chatGroupList.AddNewGroup(group);
+                }
+            }
+            return chatGroupList;
+        }
+
+        public void RemoveGroupByGroupId(string id)
+        {
+            foreach (var group in GroupList)
+            {
+                if (group.HasId(id))
+                {
+                    GroupList.Remove(group);
+                }
+            }
+        }
+
+        public void RemoveGroupByIndex(int index)
+        {
+            GroupList.RemoveAt(index);
+        }
     }
 }
