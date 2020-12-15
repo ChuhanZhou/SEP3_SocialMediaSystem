b9fa653b77af79fd7f70768d1011c3a4822b4af1
ChuhanZhou <60944082+ChuhanZhou@users.noreply.github.com>
2020-12-15 22:21:06 +0800

    debug

--- SEP3_Client/Startup.cs
+++ SEP3_Client/Startup.cs
@@ -45,7 +45,7 @@ namespace SEP3_Client
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseBrowserLink();
+            //app.UseBrowserLink();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
